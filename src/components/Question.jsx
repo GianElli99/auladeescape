be752dae92de938656202b31ba236ee04646be6f
Gianfranco Elli <gianelli99@hotmail.com>
2022-06-25 21:04:42 -0300

    add color palette

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -26,11 +26,11 @@ export const Question = ({ questionData, onCorrectAnswer }) => {
         type="number"
         name="question"
         id="question"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
       />
       <button
         type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-2.5 text-center inline-flex items-center"
+        className="text-white bg-pizza hover:bg-pizza-600 focus:ring-4 focus:outline-none focus:ring-pizza-20 0 font-medium rounded-lg text-sm px-5 py-2.5 my-2.5 text-center inline-flex items-center"
       >
         Avanzar
         <svg
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -17,7 +17,7 @@ export const Homepage = ({ onStart }) => {
         <button
           onClick={() => onStart(true)}
           type="button"
-          className="text-lg text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 "
+          className="text-lg text-white bg-pizza hover:bg-pizza-600 focus:ring-4 focus:outline-none focus:ring-pizza-200 shadow-lg font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2 "
         >
           Comenzar 💥
         </button>
