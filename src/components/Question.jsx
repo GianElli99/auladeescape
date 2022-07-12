8047a15ba31faf48070cf66fc24d7f2ca8998141
Gianfranco Elli <gianelli99@hotmail.com>
2022-07-12 09:15:04 -0300

    add meta description and feedback on wrong answers

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const Question = ({ questionData, onCorrectAnswer }) => {
   const { id, question, answer } = questionData
+  const [wrongAnswer, setWrongAnswer] = useState(false)
   const inputRef = useRef(null)
   const handleSubmit = (e) => {
     e.preventDefault()
     if (answer === inputRef.current?.value) {
       onCorrectAnswer()
+    } else {
+      setWrongAnswer(true)
     }
   }
   useEffect(() => {
@@ -14,7 +17,11 @@ export const Question = ({ questionData, onCorrectAnswer }) => {
   }, [])
 
   return (
-    <form action="" onSubmit={handleSubmit} className="w-full max-w-md mx-auto">
+    <form
+      action=""
+      onSubmit={handleSubmit}
+      className="w-full max-w-md mx-auto px-2"
+    >
       <label
         htmlFor="question"
         className="block mb-2 text-sm font-medium text-gray-900"
@@ -28,6 +35,9 @@ export const Question = ({ questionData, onCorrectAnswer }) => {
         id="question"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
       />
+      {wrongAnswer ? (
+        <p className="text-red-700">Oops, intenta de nuevo</p>
+      ) : null}
       <button
         type="submit"
         className="text-white bg-pizza hover:bg-pizza-600 focus:ring-4 focus:outline-none focus:ring-pizza-200 font-medium rounded-lg text-sm px-5 py-2.5 my-2.5 text-center inline-flex items-center"
